2b7e4d7aadf6e30fcdf75bd0131dec3230bef668
hayleyhu <yuehu2018@u.northwestern.edu>
2015-03-09 16:56:36 -0500

    add form

--- taxi/views.py
+++ taxi/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
 from .forms import TaxiGroupForm
+import datetime
 
 def index(request):
-	return render(request, 'index.html', {})
+    return render(request, 'index.html', {})
 
 def matchtime(request):
-	form = TaxiGroupForm()
-	return render(request, 'matchtime.html', {'form': form})
+    form = TaxiGroupForm()
+    if request.method == "POST":
+        form = TaxiGroupForm(request.POST)
+        Group = form.save(commit = False)
+        Group.date = datetime.datetime.now()
+        Group.time = datetime.datetime.now()
+        Group.num_ppl = 2
+
+        Group.save()
+    else:
+        form = TaxiGroupForm()
+
+    return render(request, 'matchtime.html', {'form': form})
 
 
 def groupinfo(request):
-	return render(request, 'groupinfo.html',{})
+    return render(request, 'groupinfo.html',{})
